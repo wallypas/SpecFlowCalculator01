c4fe499b5cff7a669ecb78b8cf1a9ca3b481071c
wallypas <84441375+wallypas@users.noreply.github.com>
2024-06-17 05:51:18 +0200

    first commit

---
+++ SpecFlowCalculator01/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace SpecFlowCalculator01;
+
+public class Calculator
+{
+    public int FirstNumber { get; set; }
+    public int SecondNumber { get; set; }
+
+    public int Add()
+    {
+        throw new NotImplementedException();
+    }
+}
+
